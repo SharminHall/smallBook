febee1c6d065f2ff65013a130c37dc4cc1ec0e92
sharmin <sharminh@163.com>
2019-04-28 17:36:16 +0800

    router

--- src/index.js
+++ src/index.js
@@ -2,9 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Comment from './views/Comment/Comment'
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter, Route } from 'react-router-dom'
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Route path="/" exact component={App} ></Route>
+    <Route path="/comment" component={Comment} ></Route>
+  </BrowserRouter>, 
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
